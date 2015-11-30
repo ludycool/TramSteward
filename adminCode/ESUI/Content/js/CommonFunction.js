27977c454fcef5759ad81e86dc1b1cbe250fdad2
ludycool <ludycool@81f30268-d1d5-4ef6-bcda-4156f969636f>
2015-11-30 15:34:21 +0000

    表单样式

--- adminCode/ESUI/Content/js/CommonFunction.js
+++ adminCode/ESUI/Content/js/CommonFunction.js
@@ -1,4 +1,17 @@
-﻿
+﻿function FormStripes() { //表单样式
+
+    //表单样式 start
+    $(".stripes tr").mouseover(function () {
+        //如果鼠标移到class为stripe的表格的tr上时，执行函数 
+        $(this).addClass("over");
+    }).mouseout(function () {
+        //给这行添加class值为over，并且当鼠标一出该行时执行函数 
+        $(this).removeClass("over");
+    }) //移除该行的class 
+    $(".stripes tr:even").addClass("alt");
+    //给class为stripe的表格的偶数行添加class值为alt 
+    //表单样式 end
+}
 function ConvertToTreeData(data) {//转为tree所须的格式
     $.each(data, function (i, n) {
         n = ConvertToTreeDataSon(n);
