d17a9e926e0a935d361bc3b1d3f80d0356755893
ludycool <ludycool@81f30268-d1d5-4ef6-bcda-4156f969636f>
2016-01-04 05:12:15 +0000

    下载

---
+++ trunk/adminCode/ESUI/appweb/dowlown/js/libs/tongji.js
@@ -0,0 +1,9 @@
+var _hmt = _hmt || [];
+(function() {
+  'use strict';
+  var hm = document.createElement('script');
+  hm.src = '../../../hm.baidu.com/hm.js-1d41b23babfe5ed5fde77223665d7f5d'/*tpa=http://hm.baidu.com/hm.js?1d41b23babfe5ed5fde77223665d7f5d*/;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+
